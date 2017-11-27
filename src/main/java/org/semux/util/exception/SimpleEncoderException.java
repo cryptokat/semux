674e34cc705cf09f2d10782a003c2741e7a706c5
cryptokat <32390172+cryptokat@users.noreply.github.com>
2017-11-28 04:42:26 +0800

    Util: add SimpleEncoderException
    resolve issues on https://scrutinizer-ci.com/g/semuxproject/semux/issues/develop/files/src/main/java/org/semux/util/SimpleEncoder.java

---
+++ src/main/java/org/semux/util/exception/SimpleEncoderException.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2017 The Semux Developers
+ *
+ * Distributed under the MIT software license, see the accompanying file
+ * LICENSE or https://opensource.org/licenses/mit-license.php
+ */
+package org.semux.util.exception;
+
+public class SimpleEncoderException extends RuntimeException {
+
+    public SimpleEncoderException(Throwable throwable) {
+        super(throwable);
+    }
+}
